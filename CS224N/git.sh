1af43540a65427bae986de6387e71c0d1dfd13f2
Unknown <remi.dupont@utt.fr>
2018-10-21 12:20:21 +0200

    initial commit

---
+++ CS224N/git.sh
@@ -0,0 +1,5 @@
+#Don't pay any attention to this file, it is to automate my git push actions.
+
+git add .
+git commit -m "CS224n"
+git push
--- Hey/README.md
+++
@@ -1,3 +0,0 @@
-# Natual_Language_processing_code
-
-This code cover how to run a deep learning NLP model.
